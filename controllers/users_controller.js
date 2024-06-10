812cb1d3cb5f57999bb15f477e559defa405a394
Akshay Chikane <SEZ5976@neilsoft.in>
2024-06-10 14:48:27 +0530

    Added basic routing for home and user profile using ejs

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,7 @@
 module.exports.profile = function (req, res) {
-    res.end('<h1>User Profile</h1>')
+    // res.end('<h1>User Profile</h1>')
+
+    return res.render('users', {
+        title: "Profile"
+    })
 }
