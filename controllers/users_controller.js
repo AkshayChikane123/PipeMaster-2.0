db23d9707e856cc4b9dca5ca2aeafc9c2d37ac18
Akshay Chikane <SEZ5976@neilsoft.in>
2024-06-10 15:51:36 +0530

    Added library : express-ejs-layout for layouts, added partials and layouts

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,9 @@
 module.exports.profile = function (req, res) {
     // res.end('<h1>User Profile</h1>')
 
-    return res.render('users', {
-        title: "Profile"
-    })
-}
+    return res.render('../views/user_profile.ejs', {
+        title: "User Profile",
+        
+        
+    });
+};
